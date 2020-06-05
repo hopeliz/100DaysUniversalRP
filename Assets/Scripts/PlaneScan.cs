bd4f412e86dc12a32ad18c59723beb1c7be0883a
Hope Moore <hopemoore@gmail.com>
2020-06-05 19:05:59 -0400

    Add files

---
+++ Assets/Scripts/PlaneScan.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScan : MonoBehaviour
+{
+    public Transform plane;
+    public Vector3 planeOrigin;
+    public float limit = 10;
+    public float scanSpeed = 5;
+    public bool scanning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planeOrigin = plane.position;
+        scanning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scanning)
+        {
+            plane.position = new Vector3(plane.position.x - Time.deltaTime * scanSpeed, plane.position.y, plane.position.z);
+
+            if (plane.position.x <= planeOrigin.x - limit)
+            {
+                scanning = false;
+                plane.position = planeOrigin;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            scanning = true;
+            plane.position = planeOrigin;
+        }
+    }
+}
---
+++ Assets/Scripts/SphereScan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScan : MonoBehaviour
+{
+    public Transform sphere;
+    public float minSize = 0;
+    public float maxSize = 10;
+    public float expandSpeed = 2;
+    public float expandCountdown = 1;
+    public bool expanding = false;
+
+    void Start()
+    {
+        sphere.localScale = new Vector3(0, 0, 0);
+        expanding = false;
+    }
+
+    void Update()
+    {
+        if (expanding)
+        {
+            sphere.localScale = new Vector3(sphere.localScale.x + Time.deltaTime * expandSpeed, sphere.localScale.y + Time.deltaTime * expandSpeed, sphere.localScale.z + Time.deltaTime * expandSpeed);
+            
+            if (sphere.localScale.x >= maxSize)
+            {
+                expanding = false;
+                sphere.localScale = new Vector3(minSize, minSize, minSize);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            expanding = true;
+            sphere.localScale = new Vector3(minSize, minSize, minSize);
+        }
+    }
+}
