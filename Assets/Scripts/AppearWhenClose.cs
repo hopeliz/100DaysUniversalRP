7780f210a4b144250a1c35b196d5b783610fe58b
Hope Moore <hopemoore@gmail.com>
2020-06-07 02:53:05 -0400

    fix things

---
+++ Assets/Scripts/AppearWhenClose.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearWhenClose : MonoBehaviour
+{
+    public Transform objectTriggeredBy;
+    public GameObject appearingObject;
+    public float apearRate = 0.1F;
+    public float appearCountdown = 1;
+    public bool appearing = false;
+    public bool disappearing = false;
+    public bool visible = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(appearingObject.GetComponent<Renderer>().material.GetFloat("Vector1_47645657"));
+
+        gameObject.GetComponent<Renderer>().material.SetFloat("Vector1_47645657", Vector3.Distance(transform.position, objectTriggeredBy.position) / 10 - 3);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public float rotateSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + 1 * rotateSpeed * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y - 1 * rotateSpeed * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+        }
+        
+    }
+}
