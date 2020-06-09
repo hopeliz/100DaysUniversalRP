168667d5fbe14a25992f54d689b96f8460e610ec
Hope Moore <hopemoore@gmail.com>
2020-06-09 02:25:48 -0400

    Try to add Day 29
    There were issues and the computer deleted the shader never to be found.

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject blah;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blah.transform.Translate(Vector3.up);
+    }
+}
