fe97924057584b7aada08113042952bd577233d9
Hope Moore <hopemoore@gmail.com>
2020-08-19 13:17:12 -0400

    Add Day 100

---
+++ Assets/Scripts/PlayerMotor2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor2 : MonoBehaviour
+{
+    public Transform player;
+    public Transform camHolder;
+    public GameObject selectedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Physics.Raycast(camHolder.position, camHolder.TransformDirection(Vector3.forward) * 10000, out RaycastHit hit, Mathf.Infinity))
+        {
+            print(hit.transform.name);
+            Debug.DrawRay(camHolder.position, camHolder.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+            if (hit.transform.tag == "Selectable Object")
+            {
+                selectedObject = hit.transform.gameObject;
+            }
+            else
+            {
+                selectedObject = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SelectedBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedBehavior : MonoBehaviour
+{
+    public PlayerMotor2 PlayerMotor2;
+    public Material defaultMaterial;
+    public Material selectedMaterial;
+    public bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerMotor2.selectedObject != null)
+        {
+            if (PlayerMotor2.selectedObject.name == transform.name)
+            {
+                selected = true;
+            }
+            else
+            {
+                selected = false;
+            }
+
+            if (selected)
+            {
+                selectedMaterial.SetColor("Main_Color", defaultMaterial.color);
+                GetComponent<MeshRenderer>().material = selectedMaterial;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material = defaultMaterial;
+            }
+        }
+        
+    }
+}
